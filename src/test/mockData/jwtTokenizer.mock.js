7a4fc5fe18ae779f39693ee08ec62c481aa778c0
Emmanuel HAKORIMANA <hakoemmy.2017@gmail.com>
2020-02-11 11:01:28 +0200

    feature(jwt): add jwt access
    - install jsonwebtoken module
    - add encode function
    - add decode function
    - add unit tests
    [Maintains #170947432]

---
+++ src/test/mockData/jwtTokenizer.mock.js
@@ -0,0 +1,6 @@
+
+export const mockToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im5mZm5Abm5uLmNvbSIsInVzZXJJZCI6ImprZmtuZm5mIiwiaWF0IjoxNTgxNDEwMDM4LCJleHAiOjE1ODIwMTQ4Mzh9.2jW0etuZ_kVzNF-5klEKre8BgOvHGVYfHgzfaXbJjXQ';
+export const claims = {
+  email: 'octopus@andela.com',
+  userId: 'e80a1be0-428d-11ea-9434-ab08a1e03ed1'
+};
