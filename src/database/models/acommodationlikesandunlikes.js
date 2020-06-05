ab90e021d66efb02e09a88394b1a3daf89dcb613
Abdoul <abdoulniyigena@gmail.com>
2020-06-05 11:42:10 +0200

    fix return on get all accommodations

--- src/database/models/acommodationlikesandunlikes.js
+++ src/database/models/acommodationlikesandunlikes.js
@@ -1,14 +1,23 @@
-export default(sequelize, DataTypes) => {
-  const AcommodationLikesAndUnlikes = sequelize.define('AcommodationLikesAndUnlikes', {
-    userId: DataTypes.STRING,
-    accommodationId: DataTypes.STRING,
-  }, {});
+export default (sequelize, DataTypes) => {
+  const AcommodationLikesAndUnlikes = sequelize.define(
+    "AcommodationLikesAndUnlikes",
+    {
+      userId: DataTypes.STRING,
+      accommodationId: DataTypes.STRING,
+    },
+    {}
+  );
   AcommodationLikesAndUnlikes.associate = (models) => {
     // associations can be defined here
     AcommodationLikesAndUnlikes.belongsTo(models.Users, {
-      foreignKey: 'userId',
-      onDelete: 'CASCADE',
-      onUpdate: 'CASCADE',
+      foreignKey: "userId",
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+    });
+    AcommodationLikesAndUnlikes.belongsTo(models.Accommodations, {
+      foreignKey: "accommodationId",
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
     });
   };
   return AcommodationLikesAndUnlikes;
